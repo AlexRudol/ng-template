4e85f9cf069abd3a5790a9e00b80ba13c1c54888
Alex Rudol <alex.a.rudol@gmail.com>
2017-11-02 21:56:58 +1300

    Minor updates

--- src/main.aot.ts
+++ src/main.aot.ts
@@ -1,6 +1,7 @@
 import { platformBrowser } from "@angular/platform-browser";
 import { enableProdMode } from '@angular/core';
 import { AppModule } from "./app/app.module";
+import 'hammerjs';
 
 enableProdMode();
 platformBrowser().bootstrapModule(AppModule);
